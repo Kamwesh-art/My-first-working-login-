79f4decfe531b76ad39e7ca84275f4963649d677
Kamwesh-art <9274lucy@gmail.com>
2026-08-12 10:09:12 +0300

    changes in navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -10,64 +10,61 @@
         </v-text>
         <v-spacer></v-spacer>
         <v-list>
-                 <v-list-item to="/profile"
->
-                <template #prepend >
-                    <v-icon>mdi-account</v-icon>
-                </template>
 
-                <template #title>
-                    Profile 
-                </template> 
+                <v-list-item to="/profile">
+                    <template #prepend >
+                        <v-icon>mdi-account</v-icon>
+                    </template>
+                    <template #title>
+                        Profile 
+                    </template> 
 
-            </v-list-item to="/profile"
->
-               <v-list-item to="/profile"
->
-                <template #prepend>
-                    <v-icon>mdi-home</v-icon>
-                </template>
+                </v-list-item to="/profile">
+                <v-list-item to="/home"
+    >
+                    <template #prepend>
+                        <v-icon>mdi-home</v-icon>
+                    </template>
+                    <template #title>
+                        Home 
+                    </template> 
+                </v-list-item>
 
-                <template #title>
-                    Home 
-                </template> 
-            </v-list-item>
+                <v-list-item to="/possessions"
+    >
+                    <template #prepend>
+                        <v-icon>mdi-package-variant</v-icon>
+                    </template>
+                    <template #title>
+                        Possessions
+                    </template> 
+                </v-list-item>
+                
+                <v-list-item to="/tasks"
+    >
+                    <template #prepend>
+                        <v-icon>mdi-account-hard-hat</v-icon>
+                    </template>
+                    <template #title>
+                        Tasks 
+                    </template> 
+                </v-list-item>
+    
+                <v-list-item to="/checkin">
+                    <template #prepend>
+                        <v-icon>mdi-clipboard-text-clock-outline</v-icon>
+                    </template>
 
-            <v-list-item>
-                <template #prepend>
-                    <v-icon>mdi-package-variant</v-icon>
-                </template>
+                    <template #title>
+                        Check in 
+                    </template> 
+                </v-list-item>
 
-                <template #title>
-                    Possessions
-                </template> 
-            </v-list-item>
-            
-             <v-list-item>
-                <template #prepend>
-                    <v-icon>mdi-account-hard-hat</v-icon>
-                </template>
-
-                <template #title>
-                    Tasks 
-                </template> 
-            </v-list-item>
- 
-            <v-list-item>
-                <template #prepend>
-                    <v-icon>mdi-clipboard-text-clock-outline</v-icon>
-                </template>
-
-                <template #title>
-                    Checkin 
-                </template> 
-            </v-list-item>
-
-            <v-list-item
-            variant="text"
-            to="/profile"
-            text="Profile"
-            />
+                <v-list-item
+                variant="text"
+                to="/profile"
+                text="Profile"
+                />
         </v-list>
         </v-img>
     
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,23 @@
-<template>
-     <v-app>    
+<template> 
+    <v-app>   
+        <v-img 
+        src="https://imgs.search.brave.com/3ImxtaV_OQaC9i13xFswujAyJmAuW63M2lvE8gNWpjQ/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTI4/NjQ2MjI0MC9waG90/by9saWdodC1ncmV5/LWhhbmQtcGFpbnRl/ZC10ZXh0dXJlZC1i/YWNrZHJvcC1zdHVk/aW8td2FsbC5qcGc_/cz02MTJ4NjEyJnc9/MCZrPTIwJmM9X19O/SzFlTXhWcWxRT3Z4/Y3o1SklDRThKN0p6/dVdUc25lMnVIMGps/RlVHVT0"
+        cover
+        height='100vh'
+        >  
         <v-main>
-                <v-container class="fill-height">
-                    <v-row
-                        justify="center"
-                        align="center"
-                    >
-                        <v-col cols="12" class="text-center">
-                            <h1 color="white">Welcome {{ username }}</h1>
-                        </v-col>
-                    </v-row>
-                </v-container>
+            <v-container class="fill-height">
+                <v-row
+                    justify="center"
+                    align="center"
+                >
+                    <v-col cols="12" class="text-center">
+                        <h1 color="white">Welcome {{ username }}</h1>
+                    </v-col>
+                </v-row>
+            </v-container>
         </v-main>
+        </v-img>
     </v-app>
 </template>
 
