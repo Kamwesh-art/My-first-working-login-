c67e6f5ccba05cddab1797dba7a8c6862e208249
Kamwesh-art <9274lucy@gmail.com>
2026-08-13 11:49:42 +0300

    the collapsing function

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -16,6 +16,7 @@
             </v-icon>
             </v-btn>
         </template>
+
         <v-list>
 
             <!-- Profile -->
@@ -45,76 +46,88 @@
                 <v-list-item-title>
                     Logout
                 </v-list-item-title>
-
             </v-list-item>
-
         </v-list>
     </v-menu>
-    <v-navigation-drawer v-model= "drawer" permanent>
-        <v-text>
-            <h1> Kamwesh Company</h1>
-        </v-text>
-        <v-spacer></v-spacer>
+
+    <v-navigation-drawer 
+    v-model= "drawer" 
+    :rail="rail">
+        <v-list-item title="Kamwesh Company">
+            <template #append>
+                <v-btn
+                    :icon="rail ? 'mdi-chevron-right' : 'mdi-chevron-left'"
+                    variant="text"
+                    @click="rail = !rail"
+                />
+            </template>
+        </v-list-item>
         <v-list>
+            <v-list-item to="/home">
+                <template #prepend>
+                    <v-icon>mdi-home</v-icon>
+                </template>
+                <template #title>
+                    Home 
+                </template> 
+            </v-list-item>
 
-                <v-list-item to="/profile">
-                    <template #prepend >
-                        <v-icon>mdi-account</v-icon>
-                    </template>
-                    <template #title>
-                        Profile 
-                    </template> 
+            <v-list-item to="/possessions"
+>
+                <template #prepend>
+                    <v-icon>mdi-package-variant</v-icon>
+                </template>
+                <template #title>
+                    Possessions
+                </template> 
+            </v-list-item>
+            
+            <v-list-item to="/tasks"
+>
+                <template #prepend>
+                    <v-icon>mdi-account-hard-hat</v-icon>
+                </template>
+                <template #title>
+                    Tasks 
+                </template> 
+            </v-list-item>
 
-                </v-list-item to="/profile">
-                <v-list-item to="/home"
-    >
-                    <template #prepend>
-                        <v-icon>mdi-home</v-icon>
-                    </template>
-                    <template #title>
-                        Home 
-                    </template> 
-                </v-list-item>
+            <v-list-item to="/checkin">
+                <template #prepend>
+                    <v-icon>mdi-clipboard-text-clock-outline</v-icon>
+                </template>
 
-                <v-list-item to="/possessions"
-    >
-                    <template #prepend>
-                        <v-icon>mdi-package-variant</v-icon>
-                    </template>
-                    <template #title>
-                        Possessions
-                    </template> 
-                </v-list-item>
-                
-                <v-list-item to="/tasks"
-    >
-                    <template #prepend>
-                        <v-icon>mdi-account-hard-hat</v-icon>
-                    </template>
-                    <template #title>
-                        Tasks 
-                    </template> 
-                </v-list-item>
-    
-                <v-list-item to="/checkin">
-                    <template #prepend>
-                        <v-icon>mdi-clipboard-text-clock-outline</v-icon>
-                    </template>
-
-                    <template #title>
-                        Check in 
-                    </template> 
-                </v-list-item>
-            </v-list>
+                <template #title>
+                    Check in 
+                </template> 
+            </v-list-item>
+        </v-list>
     </v-navigation-drawer>
-    
-
 </template>
+
+
+
 <script setup>
-import{ref} from "vue"
+import{ref} from 'vue'
 import { useRouter } from 'vue-router'
 
-const drawer =ref(true)
+const router = useRouter()
 
+// collapsing and expansion functions 
+const drawer =ref(true)
+const rail = ref(false)
+
+const username = ref(
+    localStorage.getItem("username") || "User"
+)
+
+const logout = () => {
+
+    localStorage.removeItem("access")
+    localStorage.removeItem("refresh")
+    localStorage.removeItem("username")
+
+    router.push("/login")
+}
 
 </script>
